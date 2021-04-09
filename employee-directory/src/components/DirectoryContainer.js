bfffc12622ba4687c1697c501306371b4697c45b
Diane Furlong <dfurlongm@gmail.com>
2021-04-08 23:42:31 -0400

    setting up file structure

---
+++ employee-directory/src/components/DirectoryContainer.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react"
+import API from "../utils/API.js"
+
+class DirectoryContainer extends Component {
+    state = {
+        image:"",
+        name: "",
+        phone: "",
+        email: "",
+        dob: ""
+    }
+
+
+    render() {
+        return (
+            <Container>
+                <Row>
+                    <Col size="md-8">
+
+                    </Col>
+                </Row>
+            </Container>
+        )
+    }
+}
